c7b462b2af8026edcec8de25512950e38e961d29
Janice He <chihyan50he@gmail.com>
2023-02-02 21:38:04 +0800

    #3 migration and makefile

---
+++ db/migration/20230202132105_create_transfers.up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "transfers" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
+
+CREATE INDEX ON "transfers" ("from_account_id");
+CREATE INDEX ON "transfers" ("to_account_id");
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+
+COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
