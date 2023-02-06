1d9f8c2a3da6258e4db36e50cbfeef1bc1152981
Janice He <chihyan50he@gmail.com>
2023-02-06 19:44:18 +0800

    #22 authentication middleware

--- api/server.go
+++ api/server.go
@@ -43,15 +43,20 @@ func NewServer(config util.EnvConfig, s *db.Store) (*Server, error) {
 func (server *Server) setupRouter() {
 	router := gin.Default()
 
+	// no auth middleware is needed
 	router.POST("/users", server.createUser)
-	router.GET("/users/:username", server.getUser)
 	router.POST("/users/login", server.loginUser)
 
-	router.POST("/accounts", server.createAccount)
-	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts", server.listAccounts)
+	// add auth middleware to all apis below
+	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
 
-	router.POST("/transfers", server.createTransfer)
+	authRoutes.GET("/users/:username", server.getUser)
+
+	authRoutes.POST("/accounts", server.createAccount)
+	authRoutes.GET("/accounts/:id", server.getAccount)
+	authRoutes.GET("/accounts", server.listAccounts)
+
+	authRoutes.POST("/transfers", server.createTransfer)
 
 	server.router = router
 }
